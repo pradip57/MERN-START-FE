557014267cf8acf3f8590b5fdca1f8f82d3160eb
Pradip <dellpidarp57@gmail.com>
2024-10-24 11:45:32 +0545

    thumjbnail for image

--- fe-start/src/components/common/form/file-input/file-input.components.tsx
+++ fe-start/src/components/common/form/file-input/file-input.components.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export type FileInputComponentProps = {
   name: string;
   setValue: any;
@@ -9,21 +11,40 @@ const FileInputComponent = ({
   setValue,
   multiple = false,
 }: FileInputComponentProps) => {
+  const [thumb, setThumb] = useState<any>();
+
   const handleChange = (e: any) => {
     if (multiple) {
       setValue(name, Object.values(e.target.files));
+      setThumb(Object.values(e.target.files));
     } else {
       setValue(name, e.target.files[0]);
+      setThumb(e.target.files[0]);
     }
   };
   return (
     <>
-      <input
-        onChange={handleChange}
-        multiple={multiple}
-        type="file"
-        className="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-      />
+      <div className="flex">
+        <input
+          onChange={handleChange}
+          multiple={multiple}
+          type="file"
+          className={`mt-1 block ${
+            multiple ? "w-full" : "w-3/4"
+          } text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400`}
+        />
+      </div>
+      <div className="flex">
+        {thumb && Array.isArray(thumb) ? (
+          <>mulltiple images</>
+        ) : typeof thumb === "object" ? (
+          <>
+            <img src={URL.createObjectURL(thumb)} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 };
